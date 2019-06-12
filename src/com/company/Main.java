69740f16c114b5423ee366abe0599fe9f040dc76
Abdul Rahman <ar.nj.usa@gmail.com>
2019-06-11 23:42:09 -0400

    variable update

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        int mynumber = 2;
+        double myothernumber = 1.7938;
+        System.out.println("My number is "+ mynumber);
+        System.out.println("My other number is "+ myothernumber);
+    }
+}
